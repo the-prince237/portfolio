52b1811c4c5ba90f13a56a378129169ca673bb8d
= <=>
2024-07-18 11:01:38 +0100

    Sentry setup

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -5,7 +5,7 @@
 import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
-  dsn: "https://7b883d075caa19b41fd9b00ae313a1c6@o4506813739368448.ingest.us.sentry.io/4507222371729408",
+  dsn: "https://df46a68b51184b043b4b596802e4e43b@o4507622202081280.ingest.us.sentry.io/4507622204243969",
 
   // Adjust this value in production, or use tracesSampler for greater control
   tracesSampleRate: 1,
